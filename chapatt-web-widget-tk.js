f863fa6c4709398fb278a068c126b5f10c959768
Chase Patterson <chapatt@gmail.com>
2017-04-23 17:35:29 -0400

    Add ToggleButton functionality

--- chapatt-web-widget-tk.js
+++ chapatt-web-widget-tk.js
@@ -17,11 +17,11 @@ chapatt.Signal = {
         this.callbacks.push({"callback": callback, "userData": userData});
     },
 
-    emit: function(targetWidget) {
+    emit: function(targetWidget, signalData) {
         var self = this;
 
         this.callbacks.forEach(function(item) {
-            item.callback(targetWidget, self.name, item.userData);
+            item.callback(targetWidget, self.name, signalData, item.userData);
         });
     },
 
@@ -51,15 +51,40 @@ chapatt.Emitter = {
         }).connect(callback, userData);
     },
 
-    signalEmit: function(name) {
+    signalEmit: function(name, signalData) {
         var self = this;
 
         // if signal doesn't exist, throw error
         this.signals.find(function(item) {
             return item.name === name;
-        }).emit(self);
+        }).emit(self, signalData);
     }
 }
+
+chapatt.Value = {};
+Object.assign(chapatt.Value, chapatt.Emitter);
+Object.assign(chapatt.Value,
+{
+    initValue: function() {
+        this.initEmitter();
+        this.addSignal('valueChanged');
+    },
+
+    getValue: function() {
+        return this.value;
+    },
+
+    setValue: function(value) {
+        this.signalEmit('valueChanged', value);
+        this.value = value;
+    },
+
+    new: function() {
+        var value = Object.create(this);
+        value.initValue();
+        return value;
+    }
+});
 
 chapatt.Widget = {
     widgets: [],
@@ -84,22 +109,71 @@ Object.assign(chapatt.Button,
     buttons: [],
 
     initButton: function(element) {
-        var self = this;
-
         this.initWidget(element);
         this.initEmitter();
 
         this.buttons.push(this);
 
         this.addSignal('clicked');
-        element.addEventListener('click', function() {
-            self.signalEmit('clicked');
+        element.addEventListener('click', this.handleClick.bind(this, event));
+
+        element.addEventListener('mousedown', function(event)
+        {
+            event.preventDefault();
         });
+    },
+
+    handleClick: function(event) {
+            this.signalEmit('clicked');
     },
 
     new: function(element) {
         var button = Object.create(this);
         button.initButton(element);
         return button;
+    }
+});
+
+chapatt.ToggleButton = Object.create(chapatt.Button);
+Object.assign(chapatt.ToggleButton,
+{
+    initToggleButton: function(element) {
+        this.initButton(element);
+
+        this.value = chapatt.Value.new();
+
+        this.signalConnect('clicked', this.toggle.bind(this));
+
+        this.value.signalConnect('valueChanged', this.handleValueChanged.bind(this));
+
+        if (this.element.classList.contains('selected'))
+            this.value.setValue('selected');
+        else
+            this.value.setValue('unselected');
     },
+
+    toggle: function() {
+        if (this.value.getValue() == 'selected') {
+            this.value.setValue('unselected');
+        } else {
+            this.value.setValue('selected');
+        }
+    },
+
+    handleValueChanged: function(targetWidget, signalName, signalData) {
+        classList = this.element.classList;
+        if (signalData == 'selected') {
+            if (!classList.contains('selected'))
+                classList.add('selected');
+        } else {
+            if (classList.contains('selected'))
+                this.element.classList.remove('selected');
+        }
+    },
+
+    new: function(element) {
+        var toggleButton = Object.create(this);
+        toggleButton.initToggleButton(element);
+        return toggleButton;
+    }
 });
